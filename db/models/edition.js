ded0c954e80b9ee1d1ac96d2b05821da5d3e58dd
Brook Li <absreim@users.noreply.github.com>
2019-09-21 19:08:10 -0400

    Filled in remaining fields belonging to Edition model.

--- db/models/edition.js
+++ db/models/edition.js
@@ -34,6 +34,70 @@ const Edition = db.define('edition', {
   oversized: {
     type: Sequelize.BOOLEAN,
     allowNull: false
+  },
+  booster: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  collectorNumber: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  digital: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  flavorText: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  fullArt: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  highresImage: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  illustrationId: {
+    type: Sequelize.UUID,
+    allowNull: true
+  },
+  printedName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  printedText: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  printedTypeLine: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  promo: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  releasedAt: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  reprint: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  storySpotlight: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  textless: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  variation: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
   }
 })
 
